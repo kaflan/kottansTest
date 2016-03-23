559e1c39e447154b4e555895f35b7d83134bdc56
andrey <kaf_90@mail.ru>
2016-03-24 01:21:19 +0200

    'preloader'

--- app/scripts/controllers/pokedex.js
+++ app/scripts/controllers/pokedex.js
@@ -10,15 +10,18 @@
 angular.module('kotansApp')
   .controller('PokedexCrl', function (pocemons) {
     var self = this;
+    self.preloader = false;
     pocemons.getPocemons().then(function(res) {
       self.next = angular.copy(res.data.meta.next);
       self.pocemons = angular.copy(res.data.objects);
+      self.preloader = !self.preloader;
       // $scope.pocemons = angular.copy(res.data.objects);
     });
     self.morePocemons = function (next) {
       pocemons.getPocemons(next).then(function(res){
         self.next = angular.copy(res.data.meta.next);
         self.pocemons = angular.copy(res.data.objects);
+        self.preloader = !self.preloader;
       });
     }
     self.seePocemonDetails = function(pocemon){
